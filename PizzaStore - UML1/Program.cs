8fc23c045a2bfff1bba58ebda7186f261e8bb930
Gade <adamgadefrimann@gmail.com>
2024-02-28 21:35:48 +0100

    #Added colors

--- PizzaStore - UML1/Program.cs
+++ PizzaStore - UML1/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-               
-            Console.WriteLine("Velkommen til Big Mammas Pizza");
-            Console.ForegroundColor = ConsoleColor.Green;
-             Console.WriteLine("Alle vores priser er inkl moms");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Velkommen til Big Mammas Pizza :D");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("De bedste pizzaer i hele landet!!!");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor= ConsoleColor.Green;
             Console.WriteLine();
              Console.WriteLine("nr. 1:  - Margherita-pizza + Pepsi til 81.25kr.");
             Console.WriteLine();
@@ -17,6 +21,7 @@
             Console.WriteLine("nr. 3:  - Salat-pizza + Fanta til 106.25kr.");
             Console.WriteLine();
             Console.WriteLine("For at bestille - indtast tallet af det nummer tilhørende menuen du ønsker");
+            Console.WriteLine("Alle vores priser er inkl moms");
             Console.ForegroundColor= ConsoleColor.White;
             Console.WriteLine();
             Console.WriteLine();
